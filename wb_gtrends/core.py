2b5dc61472ef9182e15eef6a83efc9e9db1d0441
Lucas <lucas.f.husted@gmail.com>
2020-03-06 12:21:32 -0500

    Edited class and added docstring

--- wb_gtrends/core.py
+++ wb_gtrends/core.py
@@ -16,7 +16,68 @@ from pandas._config import get_option
 from pandas.io.formats import console
 
 class WBTrends:
+    """
+    This class makes an index utilizing Google Trends from keywords.
 
+    Parameters
+    ----------
+    kw_list:  list
+        The list of keywords that will be searched. If larger than 5, will create
+        benchmark term from the first term in the list.
+
+    geo: str
+        The country or place the search is conducted in, see Trends documentation.
+
+    date_start: str, optional
+        The data where the index starts in format: 'yyyy-mm-dd',
+        if none provided, then does it at the cutoff days before the date end.
+
+    date_end: str, optional
+        The data where the index ends in format: 'yyyy-mm-dd',
+        if none provided, then defaults to current day.
+
+    frequency: str, optional
+        The frequency of the index. Note that the index always pulls daily data,
+        so collapsing into larger time-frames is done by averages ex-post.
+
+    make_index: Binary, optional
+        If true, then go ahead and instantiate class to generate indices.
+        Default is True.
+
+    plot: boolean, optional
+        If True, and make_index is True, then it plots index. Default is true.
+
+
+    Returns (back to class instance)
+    -------
+    self.indices: Dataframe
+        This is the normalized indexes made from the underlying data. It is
+        the main thing returned from this function.
+
+    self.trends: Dataframe
+        It is the adjusted and combined series for each term searched.
+        You could use this to plot individual keywords in the index.
+        Will differ from raw_trends_adjusted if frequency is changed from daily.
+
+    self.raw_trends_adjusted: Dictionary
+        These are the adjusted (using overlapping timeframes) raw results
+        for each term. Index of dictionary corresponds to index of timechunks.
+        Could differ from trends if frequency is changed from daily.
+
+    self.raw_trends: Dictionary
+        These are the unadjusted raw results for each term.
+        Note: Adjustment has still been made by the benchmark term for
+        searches exceeding 5 terms.
+
+    self.adjustment_factors: Series
+        Returns the adjustment factors used on each overlapping segment.
+        [term]_1 is the mean of the term in the earlier segment.
+        [term]_2 is the mean of the term in the later segment.
+        The adjustment is [term]_1/[term_2] * segment_2
+        Note: means for each segment are bounded from below by 1, so that
+        we do not seriously alter indices.
+
+    """
     # Initialize pytrend
     pytrend = TrendReq(tz=300) # tz is the timezone offset, in this case EST
 
@@ -24,8 +85,8 @@ class WBTrends:
     cutoff = 270 - 20 # google returns max 270 values; I make it 250 just in case.
     overlap = 30 # An arbitrary length of time for the series to overlap
 
-    def __init__(self, kw_list, geo,
-                 date_start=None, date_end=None, frequency='daily'):
+    def __init__(self, kw_list, geo, date_start=None, date_end=None,
+                 frequency='daily', make_index=True, plot=True):
 
         # Input Arguments
         self.kw_list = kw_list
@@ -38,6 +99,10 @@ class WBTrends:
         self.benchmark, self.search_groups = self.get_benchmark()
         self.date_start, self.date_end = self.auto_dates()
         self.timechunks = self.get_timechunks()
+
+        # Make the index, unless False:
+        if make_index:
+            self = self.make_index(plot=plot)
 
     def __repr__(self) -> str:
 
@@ -74,7 +139,8 @@ class WBTrends:
             show_dimensions=show_dimensions,
         )
 
-        return '\n'.join(['The Calculated Indices:',buf.getvalue()])
+        return '\n'.join(['The Calculated Indices (stored in object.indices):'
+                          ,buf.getvalue()])
 
     def make_index(self,plot=True):
         """
@@ -83,40 +149,33 @@ class WBTrends:
 
         Parameters
         ----------
-        self :
-            Instance of Class.
+        self: Instance of Class.
             You must instantiate the class in a valid way to run this function.
 
-        plot :
-            Binary, optional
+        plot: Binary, optional
             If you put True then will plot indices. The default is True.
 
         Returns (back to class instance)
         -------
-        self.indices :
-            Dataframe
+        self.indices: Dataframe
             This is the normalized indexes made from the underlying data. It is
             the main thing returned from this function.
             It also is returned to self.indices.
 
-        self.trends :
-            Dataframe
+        self.trends: Dataframe
             It is the adjusted and combined series for each term searched.
             You could use this to plot individual keywords in the index.
 
-        self.raw_trends_adjusted :
-            Dictionary
+        self.raw_trends_adjusted: Dictionary
             These are the adjusted (using overlapping timeframes) raw results
             for each term. Index of dictionary corresponds to index of timechunks.
 
-        self.raw_trends :
-            Dictionary
+        self.raw_trends: Dictionary
             These are the unadjusted raw results for each term.
             Note: Adjustment has still been made by the benchmark term for
             searches exceeding 5 terms.
 
-        self.adjustment_factors :
-            Series
+        self.adjustment_factors: Series
             Returns the adjustment factors used on each overlapping segment.
             [term]_1 is the mean of the term in the earlier segment.
             [term]_2 is the mean of the term in the later segment.
@@ -194,6 +253,8 @@ class WBTrends:
         # The final thing is to add the indices
         self.indices = indices
 
+        return self
+
 
 
     def pull_timeframe(self, date_start=None, date_end=None):
@@ -205,24 +266,19 @@ class WBTrends:
 
         Parameters
         ----------
-        pytrend :
+        pytrend: class
             Must be initiated with TrendReq.
             See pytrend documentation. The default is pytrend.
-        kw_list :
-            list
+        kw_list: list
             The list of search terms (default is kw_list)
-        benchmark :
-            str
+        benchmark: str
             The benchmark term, either blank or first word in kw_list.
             The default is benchmark.
-        geo :
-            str
+        geo: str
             Country or region to run the search.
-        date_start :
-            str
+        date_start: str
             Date in %y-%m-%d format. The default is date_start.
-        date_end :
-            str, optional
+        date_end: str, optional
             Date in %y-%m-%d format. The default is date_end.
 
         Raises
